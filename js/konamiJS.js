d99a2378ecf13ba4a41a8ed30c11e0d8bd32b6b5
zaneagley <zaneagley@gmail.com>
2018-07-01 18:05:25 -0500

    added more animation and pictures to konami code

--- js/konamiJS.js
+++ js/konamiJS.js
@@ -1,9 +1,31 @@
 var secretLifeCode = [];
 var win = new Audio('sounds/wow.mp3');
-
-
-
 $('body').removeClass("flash");
+
+
+
+
+
+
+
+function makeNewPosition(){
+    var h = $(window).height();
+    var w = $(window).width();
+    var nh = Math.floor(Math.random() * h);
+    var nw = Math.floor(Math.random() * w);
+
+    return [nh,nw];
+
+}
+
+function animateDiv(){
+    var newq = makeNewPosition();
+    $('.doge').animate({ top: newq[0], left: newq[1] }, function(){
+        animateDiv();
+    });
+
+}
+
 
 
 
@@ -17,9 +39,18 @@ $("body").keydown(function (e) {
         if (extraLife === "38,38,40,40,37,39,37,39,65,66,13") {
             alert("Unlocked 30 extra lives!");
             $('body').toggleClass("flash");
-            $('.hideFrame').removeClass("hideFrame");
-            win.play();
-            $(".snoop").animate({left: '700px'}, 2000);
+            $('.hideBigFrame').toggleClass("hideBigFrame");
+            setTimeout(function () {
+                win.play();
+            }, 600);
+            setTimeout(function () {
+                $('#BigWow').css({"display": "none"});
+                $('.hideFrame').removeClass("hideFrame");
+                $(".snoop").animate({left: '1500px'}, 5000);
+                animateDiv();
+            }, 5000)
+
+
         }
     }
     if (secretLifeCode.length > 10) {
