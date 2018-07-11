e8de63285dbd10a8044658a0c98308fb44688599
zaneagley <zaneagley@gmail.com>
2018-07-11 17:11:25 -0500

    added extra feautures and few design elements

--- js/weather_map.js
+++ js/weather_map.js
@@ -3,6 +3,7 @@
     var daysShown = $('#daysToShow').val();
     var lat = '';
     var lng = '';
+    var marker = '';
 
 
     function initMap() {
@@ -12,15 +13,15 @@
         var mapOptions = {
             zoom: 10,
             center: {
-                lat: 29.4958247,
-                lng: -98.5352398
+                lat: 29.419434,
+                lng: -98.494694
             }
         };
         var map = new google.maps.Map(mapCanvas, mapOptions);
         var marker = new google.maps.Marker({
             position:  {
-                lat: 29.4958247,
-                lng: -98.5352398
+                lat: 29.419434,
+                lng: -98.494694
             },
             map: map,
             draggable:true
@@ -31,7 +32,6 @@
         google.maps.event.addDomListener(window, 'load', initMap);
         google.maps.event.addListener(marker, 'dragend', function(event){
             $.ajax("http://api.openweathermap.org/data/2.5/forecast?lat="+event.latLng.lat().toFixed(3)+"&lon="+event.latLng.lng().toFixed(3)+"&APPID=027a0cfc318b8db92387b938997021b0").done(function (data) {
-                weatherApp(data);
                 lat = marker.getPosition().lat();
                 lng = marker.getPosition().lng();
                 geocodeLatLng(geocoder, map, infowindow, data);
@@ -46,54 +46,46 @@
         });
 
 
-        // $('#submitBtn').click(function () {
-        //     geocodeAddress(geocoder, map);
-        // });
+        $('#submitBtn').click(function () {
+            geocodeAddress(geocoder, map);
+        });
 
-        // function geocodeAddress(geocoder, resultsMap) {
-        //     var address = $('#searchInput').val();
-        //     geocoder.geocode({'address': address}, function(results, status) {
-        //         if (status === 'OK') {
-        //             resultsMap.setCenter(results[0].geometry.location);
-        //             var marker = new google.maps.Marker({
-        //                 map: resultsMap,
-        //                 position: results[0].geometry.location,
-        //                 draggable:true
-        //             });
-        //              lat = marker.getPosition().lat();
-        //              lng = marker.getPosition().lng();
-        //             $.ajax("http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lng+"&APPID=027a0cfc318b8db92387b938997021b0").done(function (data) {
-        //                weatherApp(data)
-        //                // geocodeLatLng(geocoder, map, infowindow, data);
-        //             });
-        //         } else {
-        //             alert('Geocode was not successful for the following reason: ' + status);
-        //         }
-        //     });
-        // }
-        //
-        // function geocodeLatLng(geocoder, map, infowindow, data) {
-        //     var latlng = {lat: lat, lng: lng};
-        //     geocoder.geocode({'location': latlng}, function(results, status) {
-        //         if (status === 'OK') {
-        //             if (results[0]) {
-        //                 map.setZoom(9);
-        //                 var marker = new google.maps.Marker({
-        //                     position: latlng,
-        //                     map: map,
-        //                     draggable:true
-        //                 });
-        //                 infowindow.setContent(weatherMarkerInfo(data));
-        //                 infowindow.open(map, marker);
-        //             } else {
-        //                 window.alert('No results found');
-        //             }
-        //         } else {
-        //             window.alert('Geocoder failed due to: ' + status);
-        //         }
-        //     });
-        // }
 
+        function geocodeAddress(geocoder, resultsMap) {
+            var address = $('#searchInput').val();
+            geocoder.geocode({'address': address}, function(results, status) {
+                if (status === 'OK') {
+                    resultsMap.setCenter(results[0].geometry.location);
+                    marker.setPosition(results[0].geometry.location);
+                     lat = marker.getPosition().lat();
+                     lng = marker.getPosition().lng();
+                    $.ajax("http://api.openweathermap.org/data/2.5/forecast?lat="+lat+"&lon="+lng+"&APPID=027a0cfc318b8db92387b938997021b0").done(function (data) {
+                       geocodeLatLng(geocoder, map, infowindow, data);
+                    });
+                } else {
+                    alert('Geocode was not successful for the following reason: ' + status);
+                }
+            });
+        }
+
+        function geocodeLatLng(geocoder, map, infowindow, data) {
+            var latlng = {lat: lat, lng: lng};
+            geocoder.geocode({'location': latlng}, function(results, status) {
+                if (status === 'OK') {
+                    if (results[0]) {
+                        map.setZoom(9);
+                         marker.setPosition(latlng);
+                        infowindow.setContent(weatherMarkerInfo(data));
+                        infowindow.open(map, marker);
+                        weatherApp(data);
+                    } else {
+                        window.alert('No results found');
+                    }
+                } else {
+                    window.alert('Geocoder failed due to: ' + status);
+                }
+            });
+        }
         lat = marker.getPosition().lat();
         lng = marker.getPosition().lng();
         $('#daysToShow').change(function () {
@@ -148,18 +140,30 @@
             $('body').css('background-image', 'url("images/clear-bg.jpg")')
         }
         arrDays.forEach(function (data, i) {
-            output += '<div class="col forecastColor border border-dark p-0">';
-            output += '<p class="m-2">Day: ' + (i+1) + '</p><br>';
-            output += Math.round(((data.main.temp_min-273.15)*1.8)+32);
-            output += '/' + Math.round(((data.main.temp_max-273.15)*1.8)+32);
+            output += '<div class="col-xs-12 col-md forecastColor border border-dark p-0">';
+            output += '<p class="mt-2">Day: ' + (i+1) + '</p><br>';
+            if (document.getElementById("temperature").checked === true) {
+                output += '<p class="temperatureInfo m-0">' + Math.round(((data.main.temp_min - 273.15) * 1.8) + 32);
+                output += '°/' + Math.round(((data.main.temp_max - 273.15) * 1.8) + 32) + '°</p>';
+            }
             $.each(data.weather, function (i, item) {
-                output += '<br><img src="http://openweathermap.org/img/w/'+ item.icon + '.png">';
-                output += '<br><strong>' + item.main + '</strong>: ';
-                output +=  item.description;
+                if (document.getElementById("icon").checked === true) {
+                    output += '<img class="iconInfo m-0" src="http://openweathermap.org/img/w/' + item.icon + '.png">';
+                }
+                if (document.getElementById("conditions").checked === true) {
+                    output += '<p class="conditionsInfo m-0"><strong>' + item.main + '</strong>: ';
+                    output += item.description + '</p>';
+                }
             });
-            output += '<br><strong>Humidity</strong>: ' + data.main.humidity;
-            output += '<br><strong>Wind</strong>: ' + data.wind.speed;
-            output += '<br><strong>Pressure</strong> ' + data.main.pressure;
+            if (document.getElementById("humidity").checked === true) {
+                output += '<p class="humidityInfo m-0"><strong>Humidity</strong>: ' + data.main.humidity + '</p>';
+            }
+            if (document.getElementById("wind").checked === true) {
+                output += '<p class="windInfo m-0"><strong>Wind</strong>: ' + data.wind.speed + '</p>';
+            }
+            if (document.getElementById("pressure").checked === true) {
+                output += '<p class="pressureInfo m-0"><strong>Pressure</strong> ' + data.main.pressure + '</p>';
+            }
             output += '</div>';
             $("#forecast").append(output);
         });
@@ -167,19 +171,58 @@
         $("#forecast").html(output);
     }
 
+
+    // Marker Info Window
+
     function weatherMarkerInfo(data) {
         var output = '';
-        output += '<p>' + Math.round(((data.list[0].main.temp_min-273.15)*1.8)+32);
-        output += '/' + Math.round(((data.list[0].main.temp_max-273.15)*1.8)+32)+'</p>';
+        if (document.getElementById("temperature").checked === true) {
+            output += '<p class="temperatureInfo m-0">' + Math.round(((data.list[0].main.temp_min - 273.15) * 1.8) + 32);
+            output += '°/' + Math.round(((data.list[0].main.temp_max - 273.15) * 1.8) + 32) + '°</p>';
+        }
         $.each(data.list[0].weather, function (i, item) {
-            output += '<br><img src="http://openweathermap.org/img/w/'+ item.icon + '.png">';
-            output += '<br><p><strong>' + item.main + '</strong>: ';
-            output +=  item.description + '</p>';
+            if (document.getElementById("icon").checked === true) {
+                output += '<br><img class="iconInfo m-0" src="http://openweathermap.org/img/w/' + item.icon + '.png">';
+            }
+            if (document.getElementById("conditions").checked === true) {
+                output += '<p class="conditionsInfo m-0"><strong>' + item.main + '</strong>: ';
+                output += item.description + '</p>';
+            }
         });
-        output += '<br><strong>Humidity</strong>: ' + data.list[0].main.humidity;
-        output += '<br><strong>Wind</strong>: ' + data.list[0].wind.speed;
-        output += '<br><strong>Pressure</strong> ' + data.list[0].main.pressure;
+        if (document.getElementById("humidity").checked === true) {
+            output += '<br><p class="humidityInfo m-0"><strong>Humidity</strong>: ' + data.list[0].main.humidity + '</p>';
+        }
+        if (document.getElementById("wind").checked === true) {
+            output += '<br><p class="windInfo m-0"> <strong>Wind</strong>: ' + data.list[0].wind.speed + '</p>';
+        }
+        if (document.getElementById("pressure").checked === true) {
+            output += '<br><p class="pressureInfo m-0"><strong>Pressure</strong> ' + data.list[0].main.pressure + '</p>';
+        }
         return output
     }
+    //
 
 
+
+
+
+    // Display Checkboxes
+
+    $('#temperature').change(function () {
+        $('.temperatureInfo, .iconInfo').toggleClass('hideTemp');
+    });
+    $('#icon').change(function () {
+        $('.iconInfo').toggleClass('hideIcon');
+    });
+    $('#conditions').change(function () {
+        $('.conditionsInfo').toggleClass('hideCon');
+    });
+    $('#humidity').change(function () {
+        $('.humidityInfo').toggleClass('hideHum');
+    });
+    $('#wind').change(function () {
+        $('.windInfo').toggleClass('hideWind');
+    });
+    $('#pressure').change(function () {
+        $('.pressureInfo').toggleClass('hidePres');
+    });
